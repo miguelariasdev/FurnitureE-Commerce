790d5aff9e592caf533e64d890093e23bbe29bd2
miguelariasdev <73453206+miguelariasdev@users.noreply.github.com>
2025-10-20 17:10:23 +0200

    product card and details + routes

--- Frontend/src/app/routes/Routes.tsx
+++ Frontend/src/app/routes/Routes.tsx
@@ -18,4 +18,12 @@ export const router = createBrowserRouter([
             {path: '/contact', element: <ContactPage />},
         ]
     }
-])
+], {
+    future: {
+        v7_relativeSplatPath: true,
+        v7_fetcherPersist: true,
+        v7_normalizeFormMethod: true,
+        v7_partialHydration: true,
+        v7_skipActionErrorRevalidation: true
+    }
+})
