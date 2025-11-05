30d2cbe67bd29037f83c8392df41b6d793e1c909
miguelariasdev <73453206+miguelariasdev@users.noreply.github.com>
2025-11-06 00:21:18 +0100

    testing de error api

--- Frontend/src/features/about/AboutPage.tsx
+++ Frontend/src/features/about/AboutPage.tsx
@@ -1,5 +1,20 @@
+import { Button, ButtonGroup, Container, Typography } from "@mui/material";
+import { useLazyGet400ErrorQuery } from "./errorApi";
+
 export default function AboutPage() {
+const [trigger400Error] = useLazyGet400ErrorQuery();
+
   return (
-    <div>AboutPage</div>
+    <Container maxWidth='lg'>
+      <Typography gutterBottom variant="h3">Error for testing</Typography>
+      <ButtonGroup fullWidth>
+        <ButtonGroup>
+          <Button variant="contained" onClick={() => trigger400Error()
+              .catch(err => console.log(err))}>
+            Test 400 Error
+          </Button>
+        </ButtonGroup>
+      </ButtonGroup>
+    </Container>
   )
-}
+};
